cf92d5ffc37e19ca5757073112459f0caed0b4ab
Ardatik <belogurovigor9@gmail.com>
2026-02-26 01:53:25 +0300

    add serializer for patient

---
+++ main/serializers/patient_serializer.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .base_person_serializer import PersonSerializer
+from main.models import Patient
+from django.contrib.auth.hashers import make_password
+
+
+class PatientSerializer(PersonSerializer):
+    class Meta(PersonSerializer.Meta):
+        model = Patient
+
+    def create(self, validated_data):
+        if "password" in validated_data:
+            validated_data["password"] = make_password(validated_data["password"])
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if "password" in validated_data:
+            validated_data["password"] = make_password(validated_data["password"])
+        return super().update(instance, validated_data)
