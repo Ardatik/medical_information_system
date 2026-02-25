fc37772f449cee67a4e987560275ff753f0231c1
Ardatik <belogurovigor9@gmail.com>
2026-02-25 22:32:44 +0300

    edit settings

--- medical_information_system/settings.py
+++ medical_information_system/settings.py
@@ -86,7 +86,10 @@ DATABASES = {
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT"),
-    }
+    },
+    "OPTIONS": {
+        "pool": True,
+    },
 }
 
 
